f35af0232594c2c9fe8658aa1a0b30af971e818f
origin <daxxx5428@naver.com>
2022-11-13 11:46:51 +0900

    entity init

--- src/main/java/com/sympl/junit_project/domain/Book.java
+++ src/main/java/com/sympl/junit_project/domain/Book.java
@@ -1,4 +1,29 @@
 package com.sympl.junit_project.domain;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+
+@Getter
+@NoArgsConstructor
+@Entity
 public class Book {
+
+    @Id @GeneratedValue( strategy = GenerationType.IDENTITY )
+    private Long id;
+
+    @Column( length = 50, nullable = false )
+    private String title;
+    @Column( length = 20, nullable = false )
+    private String author;
+
+    @Builder
+    public Book(Long id, String title, String author) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+    }
 }
