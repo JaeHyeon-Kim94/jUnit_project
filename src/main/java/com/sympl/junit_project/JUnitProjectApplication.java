6244745044671ab32c9a0b8b472d763b7bb26e04
origin <daxxx5428@naver.com>
2022-11-13 11:32:08 +0900

    Initial commit

---
+++ src/main/java/com/sympl/junit_project/JUnitProjectApplication.java
@@ -0,0 +1,13 @@
+package com.sympl.junit_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JUnitProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JUnitProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sympl/junit_project/JUnitProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sympl.junit_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JUnitProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
